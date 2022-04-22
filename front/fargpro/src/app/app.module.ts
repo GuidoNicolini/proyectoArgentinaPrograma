14475bf366dd06e90c71c41ea2d329b5c9f7a100
Guido Nicolini <guidonicolini123@hotmail.com>
2022-04-22 15:45:02 -0300

    todos los componentes creados

--- front/fargpro/src/app/app.module.ts
+++ front/fargpro/src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoComponent } from './componentes/logo/logo.component';
+import { BotonesSuperioresComponent } from './componentes/botones-superiores/botones-superiores.component';
+import { AcercaComponent } from './componentes/acerca/acerca.component';
+import { ExperienciasComponent } from './componentes/experiencias/experiencias.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EstudiosComponent } from './componentes/estudios/estudios.component';
+import { EstudioComponent } from './componentes/estudio/estudio.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { HabilidadComponent } from './componentes/habilidad/habilidad.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { ProyectoComponent } from './componentes/proyecto/proyecto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    BotonesSuperioresComponent,
+    AcercaComponent,
+    ExperienciasComponent,
+    ExperienciaComponent,
+    EstudiosComponent,
+    EstudioComponent,
+    HabilidadesComponent,
+    HabilidadComponent,
+    ProyectosComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
